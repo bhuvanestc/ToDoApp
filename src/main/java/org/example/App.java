706ea95a4cbfb35630ecb110ce9ffd433b82b4fa
bhuvan <bhuvanapraap@gmail.com>
2023-08-15 09:29:46 +0200

    wrote todoitemtask class

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,5 +3,9 @@ package org.example;
 public class App {
     public static void main(String[] args) {
 
+        Person peter = new Person();
+
+        System.out.println(peter.getSummary());
+
     }
 }
--- src/main/java/org/example/TodoItemTask.java
+++ src/main/java/org/example/TodoItemTask.java
@@ -1,2 +1,64 @@
-package org.example;public class TodoItemTask {
+package org.example;
+
+public class TodoItemTask {
+    private int id;
+    private Boolean assigned;
+    private TodoItem todoItem;
+    private Person assignee;
+
+    public TodoItemTask(int id, Boolean assigned, TodoItem todoItem, Person assignee) {
+        this.id = id;
+        this.assigned = assigned;
+        this.todoItem = todoItem;
+        this.assignee = assignee;
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Boolean isAssigned() {
+        return assigned;
+    }
+
+    public void setAssigned(Boolean assigned) {
+        this.assigned = assigned;
+    }
+
+    public TodoItem getTodoItem() {
+        return todoItem;
+    }
+
+    public void setTodoItem(TodoItem todoItem) {
+        this.todoItem = todoItem;
+    }
+    public Person getAssignee() {
+        return assignee;
+    }
+
+    public void setAssignee(Person assignee)
+    {
+        this.assignee = assignee;
+    }
+
+    public String getSummary(){
+        return this.getSummary();
+    }
+
+    @Override
+    public String toString() {
+        return "TodoItemTask{" +
+                "id=" + id +
+                ", assigned=" + assigned +
+                ", todoItem=" + todoItem +
+                ", assignee=" + assignee +
+                '}';
+    }
+
+
+
+
+
+
 }
